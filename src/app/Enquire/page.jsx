eaa958bdf850a65dad46741332d86f9f1950dd63
404manveer <manveershah007@gmail.com>
2025-12-02 15:48:18 +0530

    f

--- src/app/Enquire/page.jsx
+++ src/app/Enquire/page.jsx
@@ -198,7 +198,7 @@ export default function EnquiryNow() {
 
           ></iframe>
 
-          <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1210.8995650425923!2d75.85851741170251!3d30.860321921702237!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a82892cb07ba9%3A0xd60cc1725d6aa480!2sHPS%20Eye%20Hospital!5e0!3m2!1sen!2sin!4v1764670456672!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+     
         </div>
       </section>
     </>
